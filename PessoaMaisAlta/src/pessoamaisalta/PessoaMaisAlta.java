64cb426a669d80cee07afc368b0d8dbcf78787df
Valter <valteralvees@outlook.com>
2022-04-24 15:22:57 -0300

    Pessoa mais alta - Exercício

---
+++ PessoaMaisAlta/src/pessoamaisalta/PessoaMaisAlta.java
@@ -0,0 +1,21 @@
+package pessoamaisalta;
+import java.util.Scanner;
+
+public class PessoaMaisAlta {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int altura, comparaAltura=0, contador=0;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        do{
+            System.out.println("Qual a sua altura em CM? (Digite 0 para parar) ---");
+            altura = teclado.nextInt();
+                if (altura> comparaAltura ){
+                    comparaAltura = altura;
+                }      
+        }while(altura > 0);
+        System.out.println("A maior das alturas é a de " + comparaAltura+" cm.");
+    }
+    
+}
